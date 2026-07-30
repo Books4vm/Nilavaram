1dd1f2a81e4da1dc6009ba066e12c2c34abb64ea
Murugan Venkat <vm8100@gmail.com>
2026-07-29 21:29:59 -0700

    Add complete Accounting Wizard navigation

--- UI.js
+++ UI.js
@@ -95,7 +95,7 @@ function buildConnectionsBootstrap_() {
       .getProperty('MICROSOFT_REFRESH_TOKEN')
   );
   return {
-    build: 15,
+    build: 16,
     accessInfo: buildStorageAccessInfo_(),
     hasRefreshToken: hasRefreshToken,
     microsoftStatus: {
