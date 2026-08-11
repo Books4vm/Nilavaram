ffaa9213e7c6f8a987eab21417c26d5ffa1860bb
Books4vm <161768087+Books4vm@users.noreply.github.com>
2026-08-11 14:37:51 -0700

    Add searchable ACODE dropdowns

--- UI.js
+++ UI.js
@@ -95,7 +95,7 @@ function buildConnectionsBootstrap_() {
       .getProperty('MICROSOFT_REFRESH_TOKEN')
   );
   return {
-    build: 51,
+    build: 52,
     accessInfo: buildStorageAccessInfo_(),
     hasRefreshToken: hasRefreshToken,
     microsoftStatus: {
