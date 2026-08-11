e8d39e4e18b740a8f6fc8490cc2f9ff1af12e211
Books4vm <161768087+Books4vm@users.noreply.github.com>
2026-08-11 14:29:13 -0700

    Verify new ACODE availability before closing

--- UI.js
+++ UI.js
@@ -95,7 +95,7 @@ function buildConnectionsBootstrap_() {
       .getProperty('MICROSOFT_REFRESH_TOKEN')
   );
   return {
-    build: 50,
+    build: 51,
     accessInfo: buildStorageAccessInfo_(),
     hasRefreshToken: hasRefreshToken,
     microsoftStatus: {
